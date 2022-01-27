2a8df3b96e6f670c4070d90d55d5fbded8655ca1
Wojciech <wojtek2288@gmail.com>
2022-01-28 00:51:28 +0100

    Add pending migrations

--- RedditAPI/Startup.cs
+++ RedditAPI/Startup.cs
@@ -42,8 +42,10 @@ namespace RedditAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, RedditDbContext context)
         {
+            context.Database.Migrate();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
