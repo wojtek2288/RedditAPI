8ac6c78d6d215cdfb849188a723fcc44155cdaae
Wojciech <wojtek2288@gmail.com>
2022-01-28 01:22:52 +0100

    Add summaries to functions

--- RedditAPI/Services/RedditService.cs
+++ RedditAPI/Services/RedditService.cs
@@ -30,7 +30,7 @@ namespace RedditAPI.Services
         /// <summary>
         /// Gets an url of random image from subreddit specified in configuration
         /// </summary>
-        /// <param name="limit">Number of posts per request (Max = 100)</param>
+        /// <param name="limit">Number of posts per request (from 1 to 100)</param>
         /// <param name="sort">Post sorting method. Possible values = {relevance, hot, top, new, comments}</param>
         /// <returns>Url of random image</returns>
         public async Task<string> GetImage(int limit, string sort)
@@ -41,7 +41,7 @@ namespace RedditAPI.Services
             string subreddit = _configuration["Subreddit"];
             List<int> numbers = new List<int>();
             InitList(numbers, limit);
-            string Image = null;
+            string image = null;
 
             RestClient Client = new RestClient("http://www.reddit.com/r/" + subreddit + ".json");
             RestRequest Request = new RestRequest();
@@ -73,17 +73,18 @@ namespace RedditAPI.Services
                 //Foreach post per request
                 for (int i = 0; i < limit; i++)
                 {
+                    //Get random index
                     int selectedIdx = SelectIdx(numbers);
 
                     //Break when first image was found
                     if (body.data.children[selectedIdx].data.preview is not null)
                     {
-                        Image = body.data.children[selectedIdx].data.preview.images[0].source.url;
+                        image = body.data.children[selectedIdx].data.preview.images[0].source.url;
                         break;
                     }
                 }
 
-                if (Image is not null)
+                if (image is not null)
                     break;
 
                 InitList(numbers, limit);
@@ -91,16 +92,20 @@ namespace RedditAPI.Services
 
             History hist = new History()
             {
-                Url = Image,
+                Url = image,
                 DrawDate = DateTime.Now
             };
 
             _context.DrawHistory.Add(hist);
             if(_context.SaveChanges() != 1) throw new InternalServerErrorException("Couldn't save to Database");
             
-            return Image;
+            return image;
         }
 
+        /// <summary>
+        /// Gets all history items from Database
+        /// </summary>
+        /// <returns>History items</returns>
         public IEnumerable<History> GetHistory()
         {
             foreach(var historyItem in _context.DrawHistory)
@@ -109,26 +114,31 @@ namespace RedditAPI.Services
             }
         }
 
-        private void InitList(List<int> Numbers, int limit)
+        /// <summary>
+        /// Initializes List with values from 0 to limit
+        /// </summary>
+        /// <param name="numbers">List to be initialized</param>
+        /// <param name="limit">Upper limit of initialization</param>
+        private void InitList(List<int> numbers, int limit)
         {
             for(int i = 0; i < limit; i++)
             {
-                Numbers.Add(i);
+                numbers.Add(i);
             }
         }
 
         /// <summary>
-        /// Draw an index and remove element at this index from List
+        /// Draws an index and removes element at this index from List
         /// </summary>
         /// <param name="Numbers">List of possible numbers</param>
         /// <returns>Index of the post drawn</returns>
-        private int SelectIdx(List<int> Numbers)
+        private int SelectIdx(List<int> numbers)
         {
             Random rnd = new Random();
-            int idx = rnd.Next(0, Numbers.Count);
-            int res = Numbers[idx];
+            int idx = rnd.Next(0, numbers.Count);
+            int res = numbers[idx];
 
-            Numbers.RemoveAt(idx);
+            numbers.RemoveAt(idx);
 
             return res;
         }
